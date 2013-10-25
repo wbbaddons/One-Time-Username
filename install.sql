ad434559cd9ab40d3ccc2169addf9575415da042
Maximilian Mader <maximilian.mader@gmx.de>
2013-10-25 23:41:47 +0200

    Rename database table

--- install.sql
+++ install.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS wcf1_otu_blacklist;
-CREATE TABLE wcf1_otu_blacklist (
+DROP TABLE IF EXISTS wcf1_user_otu_blacklist_entry;
+CREATE TABLE wcf1_user_otu_blacklist_entry (
 	username	VARCHAR(255)	NOT NULL PRIMARY KEY,
 	time		INT(10)		NOT NULL
 );
