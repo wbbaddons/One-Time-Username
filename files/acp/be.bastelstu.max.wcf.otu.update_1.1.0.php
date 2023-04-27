7302488f1bdc79b855d9a22fd1b6677ca6a11588
Tim Düsterhus <tim@bastelstu.be>
2023-04-27 17:48:05 +0200

    Drop obsolete update files

--- files/acp/be.bastelstu.max.wcf.otu.update_1.1.0.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (\file_exists(WCF_DIR . 'lib/acp/page/OTUListPage.class.php')) {
-    \unlink(WCF_DIR . 'lib/acp/page/OTUListPage.class.php');
-}
--- update_1.1.1.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE wcf1_user_otu_blacklist_entry CHANGE username username	VARCHAR(100)	NOT NULL;
